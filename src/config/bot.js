17aaf6c590abb93cf4571f68d3ee99f8d3959e1b
Sprogaming3704 <radekgraczminecrafta@gmail.com>
2026-06-20 10:44:23 +0200

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,10 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "W czym mogę pomóc?
+         ",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 1,
       },
     ],
   },
